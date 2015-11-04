2159f4c35cb6622efb228f656eb41e7b5f9b2d59
s-ben <seth.benton@gmail.com>
2015-11-03 20:54:55 -0800

    updating settings for production

--- subtractor/settings.py
+++ subtractor/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -143,6 +144,13 @@ RQ_QUEUES = {
     }
 }
 
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 # if DEBUG or TESTING:
 #     for queueConfig in RQ_QUEUES.itervalues():
 #         queueConfig['ASYNC'] = False
