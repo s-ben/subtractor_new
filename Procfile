6eac8d23976c443d6b9e5defe6be215b23133350
s-ben <seth.benton@gmail.com>
2015-11-05 11:22:34 -0800

    created worker.py file to instantiate RQ worker on heroku servers. Modified Procfile to call worker.py, automatically creating workers upon startup

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn subtractor.wsgi --log-file -
+worker: python worker.py
---
+++ worker.py
@@ -0,0 +1,18 @@
+import os
+from django.conf import settings # adding to try and fix error...
+
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subtractor.settings")  #adding to try and fix worker error...
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
