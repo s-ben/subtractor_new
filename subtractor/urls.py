7aecc06093d94e48762767dbcb994abc6ec11e2a
s-ben <seth.benton@gmail.com>
2015-11-05 16:03:40 -0800

    experiment

--- subtractor/urls.py
+++ subtractor/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
 	
 	
-    # url(r'^$', include('audio_process.urls')), # DELETE ONE PATH FOR AUDIO PROCESS
     url(r'^audio_process/', include('audio_process.urls')),
+    url(r'^$', include('audio_process.urls')), # DELETE ONE PATH FOR AUDIO PROCESS
     url(r'^admin/', include(admin.site.urls)),
     url(r'^django-rq/', include('django_rq.urls')) #PUT IN AUDIO_PROCESS AS WELL?
 ] 
