cc2b06388c168fc26d8b97b5dd7f2ce37412c659
s-ben <seth.benton@gmail.com>
2015-11-18 14:05:47 -0800

    refreshing heroku as experiment to get file download working

--- audio_process/views.py
+++ audio_process/views.py
@@ -36,7 +36,9 @@ from django_rq import job
 
 def download(request):
 
-
+    if request.session.test_cookie_worked():
+        print ">>>> TEST COOKIE WORKED!"
+        request.session.delete_test_cookie()
 
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
@@ -97,6 +99,10 @@ def download(request):
         
 
 def index(request):
+
+
+    request.session.set_test_cookie()
+
     # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
